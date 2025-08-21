e36795182f3f777c1aa886ae20cfe6be0666c2f4
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-21 16:32:00 +0800

    updated Ui for post and added comment funcation dialog

--- resources/js/pages/Babysitter/Index.tsx
+++ resources/js/pages/Babysitter/Index.tsx
@@ -17,12 +17,13 @@ import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { PageProps as InertiaPageProps } from '@inertiajs/core'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { CircleAlert, Megaphone, History, Heart, MessageCircleMore } from 'lucide-react';
+import { CircleAlert, Megaphone, History, Heart, MessageCircleMore, EllipsisVertical, Send } from 'lucide-react';
 import { UserDisplay } from '@/components/user-display';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useInitials } from '@/hooks/use-initials';
-import  moment from 'moment';
+import moment from 'moment';
+import { useState, useEffect } from "react";
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -48,6 +49,7 @@ interface PageProps extends InertiaPageProps {
 export default function Index() {
     const { posts, flash } = usePage<PageProps>().props;
     const { auth } = usePage<SharedData>().props;
+    const [comment, setComment] = useState("");
 
     const { data, setData, post, processing, errors } = useForm({
         babysitter_id: auth.user?.id,
@@ -57,8 +59,12 @@ export default function Index() {
     const submitPost = (e: React.FormEvent) => {
         e.preventDefault();
         post(route('babysitter.store'));
+
+       data.post = '';
     }
     const getInitials = useInitials();
+
+
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
@@ -66,7 +72,7 @@ export default function Index() {
             <div className="flex h-full flex-1 flex-col gap-6 rounded-xl p-4 overflow-x-auto">
                 <div className='flex justify-start'>
                     <Dialog>
-                        <div className='bg-background rounded-lg border p-6 shadow-lg duration-200'>
+                        <div className='bg-background rounded-lg border p-6 shadow-lg duration-20 flex justify-between space-x-6'>
                             <UserDisplay />
                             <DialogTrigger asChild>
                                 <Button className='rounded-full flex justify-start p-5' variant="outline">Tell parents about yourself…</Button>
@@ -108,48 +114,72 @@ export default function Index() {
                                     <div className="grid gap-3 mb-4">
                                         <Input type='hidden' name="babysitter_id" placeholder='Tell parents about yourself…' onChange={(e) => setData('babysitter_id', parseInt(e.target.value))} value={data.post} />
 
-                                        <Input type='text' name="post" placeholder='Tell parents about yourself…' onChange={(e) => setData('post', e.target.value)} value={data.post} autoComplete='post' />
+                                        <Input id='postInput' type='text' name="post" placeholder='Tell parents about yourself…' onChange={(e) => setData('post', e.target.value)} value={data.post} autoComplete='post' />
                                     </div>
                                 </div>
                                 <DialogFooter>
-                                    <Button disabled={processing} type='submit' className='cursor-pointer'>Post</Button>
+                                    <Button disabled={processing} type='submit' className='cursor-pointer' id='postButton'>Post</Button>
                                 </DialogFooter>
                             </form>
                         </DialogContent>
                     </Dialog>
                 </div>
-                
+
                 {posts.length > 0 && (
                     <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2">
-
                         {posts.map((p) => (
-                            <div className='bg-background rounded-lg border p-6 shadow-lg duration-200'>
-                                <div>
-                                <div className='flex flex-row gap-2 items-center'>
-                                    <>
-                                        <Avatar className="h-8 w-8 overflow-hidden rounded-full">
-                                            <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                                {getInitials(p.name)}
-                                            </AvatarFallback>
-                                        </Avatar>
-                                        <div className="grid flex-1 text-left text-sm leading-tight">
-                                            <div className='flex flex-col'>
-                                                <span className="truncate font-medium">{p.name}</span>
-                                                <div className='flex flex-row mt-2 gap-1 items-center text-muted-foreground'> 
-                                                    <History className='w-3 h-3 '/>
-                                                    <span className='text-xs capitalize '>{moment(p.created_at).fromNow()}</span>
+                            <div className='bg-background rounded-lg border p-6 shadow-lg duration-200 min-h-[200px] flex flex-col' key={p.id}>
+                                <div className='flex justify-between'>
+
+                                    <div className='flex flex-row gap-2 items-center'>
+                                        <>
+                                            <Avatar className="h-8 w-8 overflow-hidden rounded-full">
+                                                <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                                    {getInitials(p.name)}
+                                                </AvatarFallback>
+                                            </Avatar>
+                                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                                <div className='flex flex-col'>
+                                                    <span className="truncate font-medium">{p.name}</span>
+                                                    <div className='flex flex-row mt-2 gap-1 items-center text-muted-foreground'>
+                                                        <History className='w-3 h-3 ' />
+                                                        <span className='text-xs capitalize '>{moment(p.created_at).fromNow()}</span>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </>
+                                        </>
+                                    </div>
+                                    <EllipsisVertical />
                                 </div>
-                            </div>
-                            <div className='text-start flex items-start justify-start mt-3'>
-                                <span>{p.post}</span>
+                                <div className='text-start flex items-start justify-start mt-3'>
+                                    <span>{p.post}</span>
                                 </div>
-                                <div className='mt-4 flex flex-row items-center gap-2'>
-                                    <Heart className='w-5 h-5' />
-                                    <MessageCircleMore className='w-5 h-5' />
+                                <div className='mt-auto flex flex-row items-center gap-2 justify-end'>
+                                    <Heart className='w-5 h-5 hover:text-red-700 dark:hover:text-red-400  transition delay-50 duration-300 cursor-pointer' />
+
+                                    <Dialog>
+                                        <form>
+                                            <DialogTrigger asChild>
+                                                <MessageCircleMore className='w-5 h-5 hover:text-blue-700 dark:hover:text-blue-400 transition delay-50 duration-300 cursor-pointer' />
+                                            </DialogTrigger>
+                                            <DialogContent className="sm:max-w-[600px]">
+                                                <DialogHeader>
+                                                    <DialogTitle>Comment something...</DialogTitle>
+                                                </DialogHeader>
+
+                                                <div className="grid gap-4">
+                                                    <div className="grid gap-3 ">
+                                                        <div className='flex justify-between space-x-2'>
+                                                            <Input name="comment" placeholder='Comment something…' value={comment} onChange={(e) => setComment(e.target.value)} />
+
+                                                            <Button className={comment.trim() === "" ? "hidden" : "hover:-translate-y-1 hover:scale-105 cursor-pointer transition delay-50 duration-300"}
+                                                                type="submit"><Send /></Button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </DialogContent>
+                                        </form>
+                                    </Dialog>
                                 </div>
                             </div>
                         ))}
