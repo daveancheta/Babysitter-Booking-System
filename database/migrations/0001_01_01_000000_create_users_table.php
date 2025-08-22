87a8501d0c30406d0a4b46c875dd819234f32933
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-22 23:11:04 +0800

    added new column for users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('address');
+            $table->bigInteger('contact_number');
+            $table->string('profile');
             $table->decimal('balance', 10, 2)->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
