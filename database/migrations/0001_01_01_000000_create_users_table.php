3b9f4c823daaf2b7c7e21845c604c2b4772b9e85
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-22 23:15:09 +0800

    updated the migration of the users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('address');
-            $table->bigInteger('contact_number');
-            $table->string('profile');
+            $table->string('address')->nullable();
+            $table->bigInteger('contact_number')->nullable();
+            $table->string('profile')->nullable();
             $table->decimal('balance', 10, 2)->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
