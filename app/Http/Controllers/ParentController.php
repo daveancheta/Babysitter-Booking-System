a460402263704a2a345616ded4d9e39121eee387
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-23 22:31:51 +0800

    updated book now page

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,7 +13,8 @@ class ParentController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Parents/Index');
+        $users = User::where('is_babysitter', 1)->get();
+        return Inertia::render('Parents/Index', compact('users'));
     }
 
     /**
--- resources/js/pages/Parents/Index.tsx
+++ resources/js/pages/Parents/Index.tsx
@@ -25,6 +25,7 @@ import { useInitials } from '@/hooks/use-initials';
 import moment from 'moment';
 import { useState, useEffect, use } from "react";
 import { parse } from 'path';
+import { StringToBoolean } from 'class-variance-authority/types';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -33,13 +34,37 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+interface Users {
+    id: number;
+    name: string; 
+    email: string;
+    address: string;
+    contact_number: string;
+    profile: string;
+}
+
+interface PageProps extends InertiaPageProps {
+    users: Users[];
+}
+
 export default function Index() {
+    const { users } = usePage<PageProps>().props;
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div id='ajax' className="flex h-full flex-1 flex-col gap-6 rounded-xl p-4 overflow-x-auto">
-              </div>
 
+                {users.length > 0 && (
+                   <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2">
+                    {users.map((u) => (
+                        <div className='bg-background rounded-lg border p-6 shadow-lg duration-200 min-h-[200px] flex flex-col' key={u.id}>
+                            <span className=''>{u.name}</span>
+                        </div>
+                    ))}
+                    </div>
+                    
+                )}
+              </div>
         </AppLayout >
     );
 }
