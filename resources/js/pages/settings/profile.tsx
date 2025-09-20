476d49cea3ee42fec2bbae14b33d10a775fa55d8
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-09-20 11:36:52 +0800

    Added a button to copy the account id value

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -10,6 +10,8 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
+import { Copy } from "lucide-react";
+import { use, useEffect } from 'react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -21,10 +23,10 @@ const breadcrumbs: BreadcrumbItem[] = [
 export default function Profile({ mustVerifyEmail, status }: { mustVerifyEmail: boolean; status?: string }) {
     const { auth } = usePage<SharedData>().props;
     let balance = auth.user.balance;
-    let balanceFormatted = new Intl.NumberFormat('en-US', {minimumFractionDigits: 2}).format(balance);
+    let balanceFormatted = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 }).format(balance);
     const balanceValue = '$' + balanceFormatted;
     let rate = auth.user.rate;
-    let rateFormatted = new Intl.NumberFormat('en-US', {minimumFractionDigits: 2}).format(rate);
+    let rateFormatted = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 }).format(rate);
     const rateValue = "$" + rateFormatted;
 
     return (
@@ -45,6 +47,26 @@ export default function Profile({ mustVerifyEmail, status }: { mustVerifyEmail: 
                     >
                         {({ processing, recentlySuccessful, errors }) => (
                             <>
+                              <div className="grid gap-2">
+                                    <Label htmlFor="balance">Account ID</Label>
+
+                                    <div className='border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                                    "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+                                    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'>
+                                        <div className='flex flex-row w-full items-center'>
+                                            <div className='select-none'>
+                                                <input
+                                                    id="account_id"
+                                                    className="select-none pointer-events-none"
+                                                    value={auth.user.account_id}
+                                                    readOnly
+                                                />
+                                            </div>
+                                                <Copy className='w-4 h-4 ms-auto cursor-pointer' onClick={() => {navigator.clipboard.writeText(auth.user.account_id)}} />
+                                        </div>
+                                    </div>
+                                </div>
+                                
                                 <div className="grid gap-2">
                                     <Label htmlFor="name">Name</Label>
 
@@ -119,16 +141,6 @@ export default function Profile({ mustVerifyEmail, status }: { mustVerifyEmail: 
                                     "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
                                     "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'>
                                         {balanceValue}
-                                    </span>
-                                </div>
-
-                                 <div className="grid gap-2">
-                                    <Label htmlFor="balance">Account ID</Label>
-
-                                    <span className='border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-                                    "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-                                    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'>
-                                        {auth.user.account_id}
                                     </span>
                                 </div>
 
