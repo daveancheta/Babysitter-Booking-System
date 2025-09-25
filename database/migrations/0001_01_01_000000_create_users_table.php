206d79255d5f6938c69ec86280898cd757303f4b
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-09-25 20:50:19 +0800

    New column for users table is_admin

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->decimal('rate', 10, 2)->default(0);
             $table->string('status')->nullable();
             $table->boolean('is_babysitter')->default(0);
+            $table->boolean('is_admin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
