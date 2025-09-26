ca8225e01002571bb884a6f60aa76f79e07bbf9e
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-09-26 14:55:31 +0800

    Added in user info the actual profile

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -1,17 +1,20 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useInitials } from '@/hooks/use-initials';
-import { type User } from '@/types';
+import { SharedData, type User } from '@/types';
+import { usePage } from '@inertiajs/react';
 
 export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: boolean }) {
     const getInitials = useInitials();
+    const { auth } = usePage<SharedData>().props;
 
     return (
         <>
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
                 <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                {auth?.user.profile.length > 0 ?  <img className='rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white object-fill' src={`${window.location.origin}/storage/${auth?.user.profile}`} alt="" /> :  <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                     {getInitials(user.name)}
-                </AvatarFallback>
+                </AvatarFallback>}
+               
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
