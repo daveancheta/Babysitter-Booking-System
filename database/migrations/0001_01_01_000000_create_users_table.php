67d485321cf5d51f3f0593e29acb3add49215d82
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-09-26 12:08:45 +0800

    fixed some issues in users table:

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->boolean('is_babysitter')->default(0);
             $table->boolean('is_admin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password') ;
             $table->rememberToken();
             $table->timestamps();
         });
