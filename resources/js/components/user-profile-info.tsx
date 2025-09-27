0d305519ad9fc1a2ce95df14e2ce1ccb5d7be139
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-09-27 23:54:36 +0800

    Added a border on profile

--- resources/js/components/user-profile-info.tsx
+++ resources/js/components/user-profile-info.tsx
@@ -13,7 +13,12 @@ export function UserProfileInfo({ user, showEmail = false }: { user: User; showE
                 <AvatarImage src={user.avatar} alt={user.name} />
                 {auth?.user.profile === null ? <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                     {getInitials(user.name)}
-                </AvatarFallback> : <img className='rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white object-cover' src={`${window.location.origin}/storage/${auth?.user.profile}`} alt=''/>}
+                </AvatarFallback> : <img
+                    className="rounded-full bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white object-cover border-4 border border-gray-900 dark:border-white"
+                    src={`${window.location.origin}/storage/${auth?.user.profile}`}
+                    alt=""
+                />
+                }
             </Avatar>
         </>
     );
