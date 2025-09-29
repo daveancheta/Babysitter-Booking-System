536411877f18a14703c1e3fb6d206164f35427e1
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-09-29 16:08:29 +0800

    Adjusted the reponsiveness in mobile

--- resources/js/pages/Main/welcome.tsx
+++ resources/js/pages/Main/welcome.tsx
@@ -17,10 +17,10 @@ interface LoginProps {
 
 export default function Welcome() {
     return (
-        <div className='justify-center flex min-h-[50vh]'>
+        <div className='justify-center flex xl:min-h-[60vh] min-h-[70vh]'>
             <Head title='welcome' />
 
-            <div className='mt-auto flex flex-row gap-5'>
+            <div className='mt-auto flex flex-row gap-5 grid gap-4 md:grid-cols-1 lg:grid-cols-2'>
                 <Link href={route('login')}>
                     <div className='bg-background rounded-lg border p-6 min-w-[300px] hover:shadow-xl transition-all duration-300 transform hover:scale-105 hover:border-red-500 border'>
                         <h1 className='text-center font-bold text-2xl'>Welcome Back</h1>
