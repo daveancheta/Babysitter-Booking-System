4067f5f7e578640a6510bf0d547c492640fe1ecf
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-01 18:45:36 +0800

    Added new feature search user UI only not finish

--- resources/js/components/breadcrumbs.tsx
+++ resources/js/components/breadcrumbs.tsx
@@ -3,11 +3,24 @@ import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
 import { Link } from '@inertiajs/react';
 import { Fragment } from 'react';
 import { Button } from './ui/button';
-import { MessageCircle, MessageCircleMore } from 'lucide-react';
+import { MessageCircle, MessageCircleMore, Search } from 'lucide-react';
+import { Input } from './ui/input';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export function Breadcrumbs({ breadcrumbs }: { breadcrumbs: BreadcrumbItemType[] }) {
+
+    const isMobile = useIsMobile();
+
+    const handleSearch = () => {
+        document.getElementById("search-input")?.classList.remove("hidden");
+        document.getElementById("linkMessage")?.classList.add("hidden");
+        document.getElementById("searchIcon")?.classList.add("hidden");
+        document.getElementById("BreadcrumbList")?.classList.add("hidden");
+    }
     return (
         <>
+        {isMobile ? 
+        <div id='BreadcrumbList' className=''>
             {breadcrumbs.length > 0 && (
                 <Breadcrumb>
                     <BreadcrumbList >
@@ -33,9 +46,57 @@ export function Breadcrumbs({ breadcrumbs }: { breadcrumbs: BreadcrumbItemType[]
 
                 </Breadcrumb>
             )}
-            <Link href="" className="absolute right-5 cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 has-[>svg]:px-3">
-                <MessageCircleMore />
-            </Link>
+            </div>
+            :
+            <div>
+            {breadcrumbs.length > 0 && (
+                <Breadcrumb>
+                    <BreadcrumbList >
+                        {breadcrumbs.map((item, index) => {
+                            const isLast = index === breadcrumbs.length - 1;
+                            return (
+                                <Fragment key={index}>
+                                    <BreadcrumbItem>
+                                        {isLast ? (
+                                            <BreadcrumbPage>{item.title}</BreadcrumbPage>
+                                        ) : (
+                                            <BreadcrumbLink asChild>
+                                                <Link href={item.href}>{item.title}</Link>
+                                            </BreadcrumbLink>
+                                        )}
+                                    </BreadcrumbItem>
+                                    {!isLast && <BreadcrumbSeparator />}
+                                </Fragment>
+                            );
+                        })}
+
+                    </BreadcrumbList>
+
+                </Breadcrumb>
+            )}
+            </div>
+            }
+            <div className="absolute right-5 cursor-pointer inline-flex items-center justify-center gap-2 ">
+                {isMobile ? 
+                <Link href="" id='linkMessage' className="whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 has-[>svg]:px-3">
+                    <MessageCircleMore />
+                </Link> : <Link href="" className="whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 has-[>svg]:px-3">
+                    <MessageCircleMore />
+                </Link>
+}
+                <div id='searchIcon'>
+                <Button onClick={handleSearch} variant='outline' className="">
+                    <Search />
+                </Button>
+                </div>
+                <div className='flex flex-row hidden gap-2 items-center' id='search-input'>
+                <div className='relative'>
+                    <Search className='absolute left-3 top-1/2 p-1 -translate-y-1/2 text-gray-400 w-5 h-5' />
+                    <Input type="text" className='pl-10' placeholder='Search...'/>
+                </div>
+                <button><Search className='h-5 w-5'/></button>
+                </div>
+            </div>
         </>
     );
 }
