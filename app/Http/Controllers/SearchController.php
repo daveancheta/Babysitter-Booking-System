8b05f9654f9b86c12543bfc7f024c438f9af642b
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-01 23:10:04 +0800

    Updated search result will only fetch the baysitter and parent user

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,7 +10,9 @@ use Inertia\Inertia;
 class SearchController extends Controller
 {
     public function __invoke() {
-        $results = User::where('name', 'LIKE', '%'.request('search').'%')->get();
+        $results = User::where('name', 'LIKE', '%'.request('search').'%')
+        ->whereNot('is_admin', true)
+        ->get();
 
         foreach($results as $r) {
             $r->createdAtFormatted = Carbon::parse($r->created_at, 'UTC')->isoFormat('MMMM Do YYYY');
