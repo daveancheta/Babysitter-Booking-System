e8a9b792667fa8dedbd563966276d57f4a798d6b
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-05 20:42:48 +0800

    Added sample users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,44 @@ class DatabaseSeeder extends Seeder
             'password' => 'superadmin12345',
             'is_admin' => true,
         ]);
+        User::factory()->create([
+            'account_id' => 'kZRVIP32He',
+            'name' => 'Joe Goldberg',
+            'email' => 'joegoldberg@gmail.com',
+            'profile' => 'profile/RLUgH6K4ceBJhL2WIodvCwB4z8IpNQlGsPlKRr8I.jpg',
+            'is_babysitter' => false,
+            'is_admin' => false,
+            'email_verified_at' => '2025-09-26 03:34:29',
+            'password' => 'goldberg1234',
+        ]);
+         User::factory()->create([
+            'account_id' => '0RRtTCZYsF',
+            'name' => 'Love Quinn',
+            'email' => 'lovequinn@gmail.com',
+            'profile' => 'profile/22YdYFzDAmRPf9CBvK5sLxczOfHKynJCI27UafSr.webp',
+            'is_babysitter' => true,
+            'is_admin' => false,
+            'email_verified_at' => '2025-09-26 03:34:29',
+            'password' => 'quinn1234',
+        ]);
+         User::factory()->create([
+            'account_id' => 'bA5rD5lH9M',
+            'name' => 'Guinevere Beck',
+            'email' => 'guineverebeck@gmail.com',
+            'profile' => 'profile/S0kgxnIBMGJxLLqBUsEddqSxWSB222cGAcjihsED.jpg',
+            'is_babysitter' => true,
+            'is_admin' => false,
+            'email_verified_at' => '2025-09-26 03:34:29',
+            'password' => 'beck1234',
+        ]);
+        User::factory()->create([
+            'account_id' => 'vH4iB3PA2N',
+            'name' => 'Dave Ancheta',
+            'email' => 'daveancheta@gmail.com',
+            'is_babysitter' => false,
+            'is_admin' => true,
+            'email_verified_at' => '2025-09-26 03:34:29',
+            'password' => 'ancheta1234',
+        ]);
     }
 }
