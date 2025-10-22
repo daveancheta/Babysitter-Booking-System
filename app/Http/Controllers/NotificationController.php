9a6ce2abe38935f1418805abfce78afdf7970a67
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-22 19:37:49 +0800

    Order by latest notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,21 +9,22 @@ use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
-    public function notificationCount() 
+    public function notificationCount()
     {
         $count = Notification::where('user_id', Auth::id())
-        ->where('is_read', false)
-        ->get()
-        ->count();
+            ->where('is_read', false)
+            ->get()
+            ->count();
 
         return response()->json($count);
     }
 
     public function notification()
     {
-         $notification = Notification::where('user_id', Auth::id())
-        ->where('is_read', false)
-        ->get();
+        $notification = Notification::where('user_id', Auth::id())
+            ->latest()
+            ->where('is_read', false)
+            ->get();
 
         foreach ($notification as $n) {
             $n->created_date = Carbon::parse($n->created_at)->diffForHumans();
