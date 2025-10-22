4471bc6eef40cc54761a1636095a7cdc3379a09d
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-22 19:36:39 +0800

    Added redirection of hire link

--- resources/js/pages/Babysitter/Index.tsx
+++ resources/js/pages/Babysitter/Index.tsx
@@ -1,7 +1,7 @@
 import { Input } from '@/components/ui/input';
 import AppLayout from '@/layouts/app-layout';
 import { SharedData, type BreadcrumbItem } from '@/types';
-import { Head, useForm, usePage } from '@inertiajs/react';
+import { Head, Link, useForm, usePage } from '@inertiajs/react';
 import {
     Dialog,
     DialogClose,
@@ -283,7 +283,7 @@ export default function Index() {
                                                                                         }} variant='outline' className='items-center cursor-pointer' disabled={processing}><UserPlus />Follow</Button>
                                                                                     </form>
                                                                                 }
-                                                                                <Button variant='outline' className='items-center cursor-pointer'><BriefcaseBusiness />Hire</Button>
+                                                                                <Link href={route('parent.index')} className="items-center cursor-pointer h-9 px-4 py-2 has-[>svg]:px-3 border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"><BriefcaseBusiness />Hire</Link>
                                                                             </div>
                                                                         </div>
                                                                     </div>
