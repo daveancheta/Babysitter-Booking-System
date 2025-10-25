f60beec42ff4ad71e5acf6d3b8999a869812595f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-25 16:09:28 +0800

    Updated the logo and system name

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,14 @@
+import { BabyIcon } from 'lucide-react';
 import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <BabyIcon className="size-5 text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Babysitter Booking</span>
+                <span className="mb-0.5 truncate leading-tight font-semibold">SitterLy</span>
             </div>
         </>
     );
