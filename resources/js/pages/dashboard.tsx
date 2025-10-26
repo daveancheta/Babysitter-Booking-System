90da64fc3ddf2ca5cc96a14377ca80921e6ade25
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-26 12:20:37 +0800

    New dashboard layout

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,30 +1,56 @@
+import { Badge } from '@/components/ui/badge';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { TrendingUp } from 'lucide-react';
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
         href: '/dashboard',
     },
-    
+
 ];
 
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+            <div className="flex flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+                <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-4">
+                    <div className='dark:bg-neutral-900 bg-background min-h-[200px] rounded-md p-7 shadow-lg flex flex-col gap-2 border'>
+                        <div className='flex justify-between items-center'>
+                            <span className='text-muted-foreground font-medium'>Total Revenue</span>
+                            <Badge variant='secondary' className='rounded-lg border dark:border-neutral-700 border-neutral-400'>
+                                <TrendingUp className="mr-1 size-4" /> +12.8%
+                            </Badge>
+                        </div>
+                        <div>
+                            <h3 className='text-3xl font-medium mt-1'>
+                                ₱1,650.00
+                            </h3>
+                        </div>
+
+                        <div className='mt-5 space-y-1'>
+                            <span className='text-sm font-medium flex flex-row gap-1 items-center text-green-600 dark:text-green-400'>
+                                Trending up this month <TrendingUp className="size-4" />
+                            </span>
+                            <span className='text-sm text-muted-foreground font-medium flex flex-row gap-1 items-center'>
+                                Revenue for this month
+                            </span>
+                        </div>
+
                     </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className='dark:bg-neutral-900 bg-background min-h-[200px] rounded-md p-7 shadow-lg flex flex-col gap-2 border'>
+
                     </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className='dark:bg-neutral-900 bg-background min-h-[200px] rounded-md p-7 shadow-lg flex flex-col gap-2 border'>
+
+                    </div>
+                    <div className='dark:bg-neutral-900 bg-background min-h-[200px] rounded-md p-7 shadow-lg flex flex-col gap-2 border'>
+
                     </div>
                 </div>
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
