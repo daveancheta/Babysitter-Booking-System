3d4816f800cddb67d24b69e2c55c39322994e525
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-26 13:12:27 +0800

    Adjusted the design

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -22,26 +22,24 @@ export default function Dashboard() {
                 <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-4">
                     <div className='dark:bg-neutral-900 bg-background min-h-[200px] rounded-md p-7 shadow-lg flex flex-col gap-2 border'>
                         <div className='flex justify-between items-center'>
-                            <span className='text-muted-foreground font-medium'>Total Revenue</span>
-                            <Badge variant='secondary' className='rounded-lg border dark:border-neutral-700 border-neutral-400'>
+                            <span className='text-muted-foreground font-medium tracking-wide'>Total Revenue</span>
+                            <Badge variant='secondary' className='rounded-lg tracking-wide border dark:border-neutral-700 border-neutral-400 text-green-600 dark:text-green-400'>
                                 <TrendingUp className="mr-1 size-4" /> +12.8%
                             </Badge>
                         </div>
                         <div>
-                            <h3 className='text-3xl font-medium mt-1'>
+                            <h3 className='text-3xl font-medium mt-1 tracking-wide'>
                                 ₱1,650.00
                             </h3>
                         </div>
-
                         <div className='mt-5 space-y-1'>
-                            <span className='text-sm font-medium flex flex-row gap-1 items-center text-green-600 dark:text-green-400'>
-                                Trending up this month <TrendingUp className="size-4" />
+                            <span className='text-sm font-medium flex flex-row gap-1 items-center text-green-600 dark:text-green-400 tracking-wide'>
+                                Revenue up this month <TrendingUp className="size-4" />
                             </span>
-                            <span className='text-sm text-muted-foreground font-medium flex flex-row gap-1 items-center'>
-                                Revenue for this month
+                            <span className='text-sm text-muted-foreground font-medium flex flex-row gap-1 items-center tracking-wide'>
+                                Total Revenue for this month
                             </span>
                         </div>
-
                     </div>
                     <div className='dark:bg-neutral-900 bg-background min-h-[200px] rounded-md p-7 shadow-lg flex flex-col gap-2 border'>
 
