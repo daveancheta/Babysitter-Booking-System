ead2295c5ed80e68a3b86cd669b0690db837d9f3
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-10-30 13:56:10 +0800

    Added autocomplete off

--- resources/js/components/breadcrumbs.tsx
+++ resources/js/components/breadcrumbs.tsx
@@ -383,7 +383,8 @@ export function Breadcrumbs({ breadcrumbs }: { breadcrumbs: BreadcrumbItemType[]
                                     className="pl-10 border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm -mr-2"
                                     placeholder='Search...'
                                     onChange={(e) => { setData('search', e.target.value); setSearch(e.target.value) }} value={data.search}
-                                    onMouseOver={handleOpenSearchContainer} />
+                                    onMouseOver={handleOpenSearchContainer} 
+                                    autoComplete='off'/>
                                 <div
                                     className={savedSearch.length === 0 ? 'hidden' : 'dark:bg-neutral-900 bg-background shadow-lg border rounded-lg min-w-[300px] min-h-[300px] absolute top-10 right-0 p-5 pr-3 hidden transition-all ease-in-out origin-top transform opacity-0 scale-0'}
                                     id='displaySavedSearch'
