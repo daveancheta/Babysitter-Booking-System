4f22cf7a6b6094bf3e73eca29a58a591d86839c5
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-12 09:46:23 +0800

    upd: .env.example for MySQL configuration and removed Vite server settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,6 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: '192.168.1.60',
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
